3a0579ef49de85bb506ca158d9c802832acf6e0a
YoonJeongbu <coma333ryu@gmail.com>
2017-03-15 14:40:22 +0900

    벨리데이션 적용, 각 메소드 테스트

--- todo.go
+++ todo.go
@@ -4,26 +4,62 @@ import (
 	"fmt"
 	"todoGolang/constants"
 	"todoGolang/databases/sqlite3"
+	"todoGolang/validaters"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	todoData := new(sqlite3.TodoData)
 
-	db := sqlite3.CreateDatabase(constants.SqliteFileInfo)
+	db := todoData.CreateDatabase(constants.SqliteFileInfo)
 	defer db.Close()
+	todoData.CreateTable(db)
 
-	sqlite3.CreateTable(db)
+	todoAddParam := sqlite3.NewTodoData("", "", false)
 
-	// todoData := new(sqlite3.TodoData)
+	vErr := validaters.CheckParam(todoAddParam, "A")
 
-	todoParam := sqlite3.TodoData{
-		Title:  "aaa",
-		DoneYN: false,
+	if vErr == nil {
+		bAddResult := todoData.AddTodoData(db, todoAddParam)
+
+		if bAddResult {
+			fmt.Println("Insert success")
+		} else {
+			fmt.Println("Insert fail")
+		}
+	} else {
+		fmt.Println(vErr)
 	}
-	sqlite3.AddTodoData(db, &todoParam)
 
-	result := sqlite3.GetTodoList(db)
+	resultTodoList := todoData.GetTodoList(db)
 
-	fmt.Println("resultresultresult", result)
+	fmt.Println("resultTodoList =========> ", resultTodoList)
+
+	todoUpdateParma := sqlite3.NewTodoData("3", "", true)
+	bUpdateResult := todoData.UpdateTodoData(db, todoUpdateParma)
+
+	if bUpdateResult {
+		fmt.Println("Update success")
+	} else {
+		fmt.Println("Update fail")
+	}
+
+	resultTodoList = todoData.GetTodoList(db)
+
+	fmt.Println("resultTodoList =========> ", resultTodoList)
+
+	todoDeleteParma := sqlite3.NewTodoData("3", "", false)
+
+	bDeleteResult := todoData.DeleteTodoData(db, todoDeleteParma)
+
+	if bDeleteResult {
+		fmt.Println("Delete success")
+	} else {
+		fmt.Println("Delete fail")
+	}
+
+	resultTodoList = todoData.GetTodoList(db)
+
+	fmt.Println("resultTodoList =========> ", resultTodoList)
 }
