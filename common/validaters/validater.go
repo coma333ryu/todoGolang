78e66079b501dc5685260933f4d0d8fbb8f4eff7
YoonJeongbu <coma333ryu@gmail.com>
2017-03-16 14:39:29 +0900

    패키지 변경

---
+++ common/validaters/validater.go
@@ -0,0 +1,27 @@
+package validaters
+
+import (
+	"errors"
+	"todoGolang/todo/model"
+)
+
+//CheckParam : validate parameter
+func CheckParam(todoParam *model.TodoData, methodType string) error {
+	if methodType == "A" {
+		if todoParam.TodoTitle() == "" {
+			return errors.New("Title is nil")
+		}
+	} else if methodType == "U" {
+		if todoParam.TodoIdx() == "" {
+			return errors.New("Idx is nil")
+		}
+	} else if methodType == "D" {
+		if todoParam.TodoIdx() == "" {
+			return errors.New("Idx is nil")
+		}
+	} else {
+		return errors.New("methodType is nil")
+	}
+
+	return nil
+}
