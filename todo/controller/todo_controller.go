389e2eca35681fc2fc328e31c83b6beaa55b6623
YoonJeongbu <coma333ryu@gmail.com>
2017-03-17 15:04:35 +0900

    컨트롤러 등록

--- todo/controller/todo_controller.go
+++ todo/controller/todo_controller.go
@@ -1,1 +1,19 @@
 package controller
+
+import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+)
+
+func Index(w http.ResponseWriter, req *http.Request) {
+	data, err := ioutil.ReadFile("public/index.html")
+	//data, err := ioutil.ReadFile("public/todo.html")
+
+	if err != nil {
+		fmt.Errorf("error reading index file : %s", err)
+	}
+	w.Header().Add("Content Type", " text/html")
+	w.WriteHeader(200)
+	w.Write(data)
+}
