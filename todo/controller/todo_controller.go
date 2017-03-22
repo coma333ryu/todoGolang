d6f9de2672904ca89514351dce7d360fef262fc6
YoonJeongbu <coma333ryu@gmail.com>
2017-03-22 14:57:00 +0900

    등록,수정,삭제 함수구현

--- todo/controller/todo_controller.go
+++ todo/controller/todo_controller.go
@@ -1,35 +1,107 @@
 package controller
 
 import (
+	"errors"
 	"html/template"
+	"log"
 	"net/http"
 	"path"
+	"strconv"
+	"todoGolang/common/exceptions"
+	"todoGolang/common/logger"
+	"todoGolang/common/validaters"
+	"todoGolang/todo/model"
+	"todoGolang/todo/services/impl"
 )
+
+var (
+	todoService   *impl.TodoDao
+	controllerLog *log.Logger
+)
+
+func init() {
+	todoService = new(impl.TodoDao)
+	controllerLog = logger.NewLogger()
+}
 
 func GetTodoList(res http.ResponseWriter, req *http.Request) {
 	view := path.Join("templates", "todo.html")
 
+	todoService := new(impl.TodoDao)
+
+	result := todoService.GetTodoList()
+	// controllerLog.Println(result)
+
 	todoTemplates := template.Must(template.ParseFiles(view))
-	todoTemplates.Execute(res, "")
+	todoTemplates.Execute(res, result)
 }
 
 func AddTodoData(res http.ResponseWriter, req *http.Request) {
-	view := path.Join("templates", "todo.html")
+	req.ParseForm()
 
-	todoTemplates := template.Must(template.ParseFiles(view))
-	todoTemplates.Execute(res, "")
+	//form value 받아내는 법
+	//req.PostFormValue("todoTitle")
+	//req.Form["todoTitle"] <== 배열이 들어옴. 그래서 아래와 같이 변경해야 함.
+	//strings.Join(req.Form["todoTitle"], "")
+
+	todoAddParam := model.NewTodoData("", req.PostFormValue("todoTitle"), false)
+
+	validErr := validaters.CheckParam(todoAddParam, "A")
+
+	if validErr != nil {
+		exceptions.ProcError(validErr)
+	}
+
+	bAddResult := todoService.AddTodoData(todoAddParam)
+
+	if bAddResult {
+		http.Redirect(res, req, "http://"+req.Host+"/", http.StatusSeeOther)
+	} else {
+		addErr := errors.New("Todo can not insert into tb_todo")
+		exceptions.ProcError(addErr)
+	}
 }
 
 func UpdateTodoData(res http.ResponseWriter, req *http.Request) {
-	view := path.Join("templates", "todo.html")
+	req.ParseForm()
 
-	todoTemplates := template.Must(template.ParseFiles(view))
-	todoTemplates.Execute(res, "")
+	isDone, _ := strconv.ParseBool(req.PostFormValue("isDone"))
+
+	todoUpdateParam := model.NewTodoData(req.PostFormValue("todoIdx"), "", isDone)
+
+	validErr := validaters.CheckParam(todoUpdateParam, "U")
+
+	if validErr != nil {
+		exceptions.ProcError(validErr)
+	}
+
+	bUpdateResult := todoService.UpdateTodoData(todoUpdateParam)
+
+	if bUpdateResult {
+		http.Redirect(res, req, "http://"+req.Host+"/", http.StatusSeeOther)
+	} else {
+		addErr := errors.New("Todo can not update into tb_todo")
+		exceptions.ProcError(addErr)
+	}
 }
 
 func DeleteTodoData(res http.ResponseWriter, req *http.Request) {
-	view := path.Join("templates", "todo.html")
+	req.ParseForm()
 
-	todoTemplates := template.Must(template.ParseFiles(view))
-	todoTemplates.Execute(res, "")
+	todoDeleteParam := model.NewTodoData(req.PostFormValue("todoIdx"), "", false)
+
+	validErr := validaters.CheckParam(todoDeleteParam, "D")
+
+	if validErr != nil {
+		exceptions.ProcError(validErr)
+	}
+
+	bDeleteResult := todoService.DeleteTodoData(todoDeleteParam)
+
+	if bDeleteResult {
+		http.Redirect(res, req, "http://"+req.Host+"/", http.StatusSeeOther)
+	} else {
+		addErr := errors.New("Todo can not delete into tb_todo")
+		exceptions.ProcError(addErr)
+	}
 }
