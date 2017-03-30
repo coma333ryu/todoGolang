8dfedbe4e759aeee5d458bccd23a36ad0548062b
YoonJeongbu <coma333ryu@gmail.com>
2017-03-30 14:56:32 +0900

    json return용 함수 추가

--- todo/services/todo_service.go
+++ todo/services/todo_service.go
@@ -2,8 +2,9 @@ package services
 
 import "todoGolang/todo/model"
 
-type TodoServicer interface {
+type TodoService interface {
 	GetTodoList() model.TodoDataList
+	GetTodoListJson() model.TodoJsonList
 	AddTodoData(addParam *model.TodoData) bool
 	UpdateTodoData(todoParam *model.TodoData) bool
 	DeleteTodoData(todoParam *model.TodoData) bool
